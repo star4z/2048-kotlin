16fd1c050cf08ddfaa95b53f0071eefdbff9bc88
Benjamin Phillips <bdphillips013@gmail.com>
2019-02-08 00:41:11 -0500

    Finished tile layout and added a couple text boxes.

--- app/src/main/java/edu/sunypoly/a2048/Index.kt
+++ app/src/main/java/edu/sunypoly/a2048/Index.kt
@@ -3,6 +3,7 @@ package edu.sunypoly.a2048
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_index.*
 
 class Index : AppCompatActivity() {
@@ -21,5 +22,20 @@ class Index : AppCompatActivity() {
         }
     }
 
-    fun shift(){}
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    fun shift(dir: Int){}
 }
