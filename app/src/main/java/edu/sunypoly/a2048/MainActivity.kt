c1eef7e94c7ca6d79f5f9435327d2876a25d93eb
Benjamin Phillips <bdphillips013@gmail.com>
2019-03-18 17:28:21 -0400

    Tiles now will swipe right once.

--- app/src/main/java/edu/sunypoly/a2048/MainActivity.kt
+++ app/src/main/java/edu/sunypoly/a2048/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -27,7 +28,11 @@ class MainActivity : AppCompatActivity() {
     private val LEFT = 2
     private val DOWN = 3
 
-    val grid = Array(4) { Array(4) { 0 } }
+    private var grid = Array(4) { Array(4) { 0 } }
+
+    private var scale = 1f
+    private val margin = (8 * scale + 0.5f).toInt()
+
 
     private var continuingGame = false
 
@@ -35,8 +40,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
 
+        scale = resources.displayMetrics.density
+
         addRandom()
         addRandom()
+
+        logBoard()
 
         touch_receiver.setOnTouchListener { _, motionEvent ->
             val action = motionEvent.action
@@ -78,6 +87,7 @@ class MainActivity : AppCompatActivity() {
                             addRandom()
                         }
 
+                        logBoard()
                         swipedAlready = true
                     }
                     true
@@ -122,24 +132,23 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun addAt(p: Position) {
-        grid[p.r][p.c] = if ((0..9).random() < 9) 2 else 4
+    private fun addAt(p: Position, value: Int = if ((0..9).random() < 9) 2 else 4) {
+        grid[p.r][p.c] = value
 
         val inflater = LayoutInflater.from(this)
 
-        val scale = resources.displayMetrics.density
 
         val tile = inflater.inflate(R.layout.tile_2, null) as TextView
-        tile.id = View.generateViewId()
+        tile.id = resources.getIdentifier("numbered_tile_${p.r}_${p.c}", "id", packageName)
         val id = tile.id
 
         with(grid[p.r][p.c]) {
 
             tile.text = toString()
             if (this < 8) {
-                tile.setTextColor(resources.getColor(R.color.brownButtonBackground))
+                tile.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.brownButtonBackground))
             } else {
-                tile.setTextColor(resources.getColor(R.color.offWhiteText))
+                tile.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.offWhiteText))
             }
 
             if (this <= 2048) {
@@ -151,14 +160,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             val defaultSize = (38 * scale + 0.5f).toInt()
-            val size = defaultSize - ((digits() - 1) * 4)
+            val size = defaultSize - ((digits() - 1) * 4)//TODO: implement variable text size
+            //ALSO nOTE: Not needed here since 4 and 2 both only have 1 digit!
 
 
             game_container.addView(tile)
         }
 
         val gridLocId = resources.getIdentifier("tile_${p.r}_${p.c}", "id", packageName)
-        val margin = (8 * scale + 0.5f).toInt()
 
         val constraintSet = ConstraintSet()
         with(constraintSet) {
@@ -189,6 +198,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun swipe(dir: Int): Boolean {
         var swiped = false
+
         when (dir) {
             LEFT -> {
                 for (i in grid.indices) {
@@ -237,11 +247,37 @@ class MainActivity : AppCompatActivity() {
                                     }
                                 }
                                 if (rightShift != 0) {
+                                    val tileId = resources.getIdentifier("numbered_tile_${i}_$j", "id", packageName)
+                                    Log.d(TAG, "tileId = $tileId")
+                                    val gridLocId = resources.getIdentifier("tile_${i}_${j + rightShift}", "id", packageName)
+                                    Log.d(TAG, "gridLocId = $gridLocId")
+
+                                    val constraintSet = ConstraintSet()
+                                    constraintSet.connect(tileId, ConstraintSet.LEFT, gridLocId, ConstraintSet.LEFT, margin)
+                                    constraintSet.connect(tileId, ConstraintSet.RIGHT, gridLocId, ConstraintSet.RIGHT, margin)
+                                    constraintSet.connect(tileId, ConstraintSet.TOP, gridLocId, ConstraintSet.TOP, margin)
+                                    constraintSet.connect(tileId, ConstraintSet.BOTTOM, gridLocId, ConstraintSet.BOTTOM, margin)
+
+                                    TransitionManager.beginDelayedTransition(game_container)
+                                    constraintSet.applyTo(game_container)
+
+                                    val tile = findViewById<TextView>(tileId)
+                                    val newId = resources.getIdentifier("numbered_tile_${i}_${j + rightShift}", "id", packageName)
+
                                     if (grid[i][j] == grid[i][j + rightShift]) {
                                         grid[i][j + rightShift] = grid[i][j] * 2
+
+                                        //Remove lower values and insert new value tile
+                                        game_container.removeView(tile)
+                                        val oldTile = findViewById<TextView>(newId)
+                                        game_container.removeView(oldTile)
+                                        addAt(Position(i, j + rightShift), grid[i][j + rightShift])
                                     } else {
                                         grid[i][j + rightShift] = grid[i][j]
                                     }
+
+                                    tile.id = newId
+
                                     grid[i][j] = 0
                                     swiped = true
                                 }
@@ -315,7 +351,77 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+
         return swiped
+    }
+
+    private fun shift(vector: Pair<Boolean, Boolean>): Boolean{
+        var swiped = false
+
+        for (i in grid.indices) {
+            for (j in grid[i].indices.reversed()) {
+                if (grid[i][j] != 0) {
+                    if (j < 3) {//last column will always be shifted as right as possible already
+                        var rightShift = 0
+                        var finishedShifting = false
+                        while (j + rightShift < 3 && !finishedShifting) {
+                            when {
+                                grid[i][j + (rightShift + 1)] == 0 -> rightShift++
+                                grid[i][j + (rightShift + 1)] == grid[i][j] -> {
+                                    rightShift++
+                                    finishedShifting = true
+                                }
+                                else -> finishedShifting = true
+                            }
+                        }
+                        if (rightShift != 0) {
+                            val tileId = resources.getIdentifier("numbered_tile_${i}_$j", "id", packageName)
+                            Log.d(TAG, "tileId = $tileId")
+                            val gridLocId = resources.getIdentifier("tile_${i}_${j + rightShift}", "id", packageName)
+                            Log.d(TAG, "gridLocId = $gridLocId")
+
+                            val constraintSet = ConstraintSet()
+                            constraintSet.connect(tileId, ConstraintSet.LEFT, gridLocId, ConstraintSet.LEFT, margin)
+                            constraintSet.connect(tileId, ConstraintSet.RIGHT, gridLocId, ConstraintSet.RIGHT, margin)
+                            constraintSet.connect(tileId, ConstraintSet.TOP, gridLocId, ConstraintSet.TOP, margin)
+                            constraintSet.connect(tileId, ConstraintSet.BOTTOM, gridLocId, ConstraintSet.BOTTOM, margin)
+
+                            TransitionManager.beginDelayedTransition(game_container)
+                            constraintSet.applyTo(game_container)
+
+                            val tile = findViewById<TextView>(tileId)
+                            val newId = resources.getIdentifier("numbered_tile_${i}_${j + rightShift}", "id", packageName)
+
+                            if (grid[i][j] == grid[i][j + rightShift]) {
+                                grid[i][j + rightShift] = grid[i][j] * 2
+
+                                //Remove lower values and insert new value tile
+                                game_container.removeView(tile)
+                                val oldTile = findViewById<TextView>(newId)
+                                game_container.removeView(oldTile)
+                                addAt(Position(i, j + rightShift), grid[i][j + rightShift])
+                            } else {
+                                grid[i][j + rightShift] = grid[i][j]
+                            }
+
+                            tile.id = newId
+
+                            grid[i][j] = 0
+                            swiped = true
+                        }
+                    }
+                }
+            }
+        }
+
+        return swiped
+    }
+
+    private fun logBoard() {
+        var s = ""
+        grid.forEach { row -> s += "\n"; row.forEach { s += "$it " } }
+        Log.v(TAG, s)
     }
 
     private fun gameIsWon(): Boolean {
@@ -359,6 +465,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         return !hasPossibleMove
+    }
+
+    fun newGame(view: View) {
+        clearViews()
+
+        grid = Array(4) { Array(4) { 0 } }
+        addRandom()
+        addRandom()
+    }
+
+    private fun clearViews() {
+        for (i in (0..3)) {
+            for (j in (0..3)) {
+                val view = findViewById<TextView>(resources.getIdentifier("numbered_tile_${i}_$j", "id", packageName))
+                view?.let { game_container.removeView(view) }
+            }
+        }
     }
 }
 
